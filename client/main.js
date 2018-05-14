86d5d1089749cf2a9791d2e01d92fc6e16026761
IllusionElements <aaronlo207@gmail.com>
2018-05-14 09:47:53 -0400

    Update main.js
    -  Refactored code & moved logic out of meteor.call into service modules
    - Fixes code smells

--- client/main.js
+++ client/main.js
@@ -1,123 +1,123 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
 var blClans = ['XGN', 'KSI', 'TSB', 'ESB', 'GRM'];
-var blk = "<h3 class='text-danger'>The friends list is blocked</h3>";
-var inv = "<h3 class='text-warning'>Invalid Gamertag/Gamertag doesn't exist</h3>";
-var check; 
 var hide = 0;
-const fetch = Meteor.call;
+const errors = new Map();
+errors.set(1007, "Invalid Gamertag/Gamertag doesn't exist!");
+errors.set(1029, 'The friends list is blocked');
+const createMessageElement = ({ className, props }) =>
+	`<h3 class='text-${className.textName}'>${props.msg}</h3>`;
+const buildError = ({ code }) =>
+	createMessageElement({
+		className: {
+			textName: 'warning',
+		},
+		props: {
+			msg: errors.get(code),
+		},
+	});
+const friendsList = (() => {
+	function isOpen({ data, searchText }) {
+		Session.set('checkVal', 1);
+		Session.set('tags', data);
+		Session.set('name', searchText);
+	}
 
+	function isBlocked({ data: { code }, searchText }) {
+		Session.set(error, `ERROR 1029: ${errors.get(code)}`);
+		Session.set('checkVal', 0);
+		Session.set(
+			'gamertag',
+			createMessageElement({
+				className: {
+					textName: 'danger',
+				},
+				props: {
+					msg: `${searchText} has their friends list blocked`,
+				},
+			})
+		);
+	}
+
+	return { isOpen, isBlocked };
+})();
+
+const profileState = (() => ({
+	invalidGamertag(data) {
+		Session.set('checkVal', 0);
+		Session.set('gamertag', buildError(data));
+	},
+}))();
+
+const setSessionState = (function() {
+	return {
+		friendsList,
+		profileState,
+	};
+})();
 // Search  Events
-
 Template.search.events({
-	'submit #search': function(f) {
-		f.preventDefault();
-		let searchText = event.target.gt_name.value;
-		hide = 1;
-		Session.set('hideVal', hide);
-		fetch('getProfile',searchText,(err, resp) => {
-			const data = resp.data;
-			if(err){
-				console.log(err);
-			} else if(data.hasOwnProperty("code")){
-				
-				Session.set('fail', inv);
+	'submit #search': function(e) {
+		e.preventDefault();
+		let searchText = event.target.gt_name.value; //grabs the text from the search box and puts it as the search text var
+		Meteor.call('searchFriends', searchText, (err, friends) => {
+			//call the xbox-live api by passing searchText as the search parameter
+			var data = friends.data;
+			Session.set('hideVal', 1);
+			if (err) {
+				Session.set('error', error);
+			} else if (data.code && data.code === 1029) {
+				//api response is different if it cannot retrieve friends; code 1029: is for blocked friends list
+				setSessionState.friendsList.isBlocked({ data, searchText });
+			} else if (data.code && data.code === 1007) {
+				//data.code: 1007 references a invalid gamertag.
+				setSessionState.profileState.invalidGamertag(data);
 			} else {
-				const profile = {
-					'name': data.gamertag,
-					'location': data.location,
-					'gamerscore': data.gamerscore,
-					'bio': data.bio,
-					'tier': data.tier,
-					'imgURL': data.avatarBodyImagePath,
-				};
-				Session.set('profile', profile);
-				console.log(profile.name);
+				//using the lookup gamertag and loading it onto session variables as last of the if loop
+				setSessionState.friendsList.isOpen({ data, searchText });
 			}
 		});
-	}
-})
+	},
+});
 
-
-Template.search.events({
- 	'submit #search': function(e){
-		e.preventDefault();
- 		let searchText = event.target.gt_name.value; //grabs the text from the search box and puts it as the search text var
- 		let blkd = "<h3 class='text-danger'>"+searchText + " has their friends list blocked</h3>"
- 		Meteor.call('searchFriends', searchText, (err, friends) => { //call the xbox-live api by passing searchText as the search parameter
- 			var data = friends.data;
- 			hide = 1;
- 			Session.set('hideVal',hide);
- 			if(err){
- 				console.log(err+"help");
- 			} else if(data.hasOwnProperty("code") && data.code == 1029){ //api response is different if it cannot retrieve friends; code 1029: is for blocked friends list
- 				check = 0;
- 				console.log("Err: "+data.code + " blocked");
- 				Session.set('checkVal', check);
- 				Session.set('gamertag', blkd); 
-				
- 			} else if(data.hasOwnProperty("code") && data.code == 1007){ //data.code: 1007 references a invalid gamertag.
- 				check = 0;
- 				console.log("invalid gt");
- 				Session.set('checkVal', check);
- 				Session.set('gamertag', "<h3 class='text-warning'>Invalid Gamertag/Gamertag doesn't exist</h3>");
- 			} else { //using the lookup gamertag and loading it onto session variables as last of the if loop
- 				check = 1;
- 				console.log(data[0]);
- 				Session.set('checkVal', check);
- 				Session.set('tags', data);
- 				Session.set('name', searchText);
- 			}
- 		})
- 	}
- })
-
-
-
-Template.registerHelper('keys', function iterate(results){ 
-  function resultMap(name) {
-    return { name: results[name].Gamertag };
-  }
-  return check === 0 ? [] : Object.keys(results).map(resultMap)
+Template.registerHelper('keys', function iterate(results) {
+	const check = Session.get('check');
+	return check === 0
+		? []
+		: Object.keys(results).map(name => ({
+				name: results[name].Gamertag,
+			}));
 });
 
 Template.friends.helpers({
-	check_if_eql_one: function() {
-		const val = Session.get('hideVal'); 
-		console.log(val);
-		return val;
+	check_if_eql_one() {
+		return Session.get('hideVal');
 	},
-	
-	friend: function() {
-		if(check !== 0) {
-			console.log(check);
-			return Session.get('tags');
-			
-		} else {
-			console.log(check);
-			return Session.get('gamertag');
-		}
-		
-	}
-	
-})
+	getCheck() {
+		return Session.get('checkVal');
+	},
+	'friends.tags'() {
+		return Session.get('tags');
+	},
+	'friends.gamertag'() {
+		return Session.get('gamertag');
+	},
+});
 
 Template.tags.helpers({
-	gamertag: function(){
+	gamertag() {
 		return Session.get('name');
-	}
-	
+	},
 });
 
 Template.layout.helpers({
-	check_if_init: function(){
-		const value = Session.get('hideVal'); 
-		console.log(value);
-		return value == 1;
-	}
-})
+	check_if_init() {
+		return Session.get('hideVal') == 1;
+	},
+});
 
-Template.profile.helpers({ profile: function(){
-	return Session.get('profile');
-}});
+Template.profile.helpers({
+	profile() {
+		return Session.get('profile');
+	},
+});
